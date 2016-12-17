dccf18f8cc752de506ef6504b8b37775809e8ac6
mozeliang <307096439@qq.com>
2016-12-17 21:31:33 +0800

    add z.core.service

---
+++ Z.Core.Service/Main/AdministratorsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Z.Core.Model.Main;
+using Z.Core.Repository.Main;
+
+namespace Z.Core.Service.Main
+{
+    public class AdministratorsService
+    {
+        IAdministratorsRepository repository = new AdministratorsRepository();
+
+        public Administrators Create()
+        {
+            var administrators = new Administrators()
+            {
+                Ad_AdminAccount="",
+                Ad_AdminPassword="",
+                Ad_CreateTime=DateTime.Now,
+                Ad_IsDetele=1,
+                Ad_IsDisable=1,
+                Ad_UpdateTime=DateTime.Now
+            };
+            repository.Create(administrators);
+            return administrators;
+        }
+    }
+}
