984da8b3a2862ab292bea62e0e3754d0e8d79bbf
mozeliang <307096439@qq.com>
2016-12-17 21:29:15 +0800

    add z.core.model

---
+++ Z.Core.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.Core.Model
+{
+    public class Class1
+    {
+    }
+}
