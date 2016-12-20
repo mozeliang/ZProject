405398b16dcfe4562f9e505557bc608f9cdda37d
mzl <307096439@qq.com>
2016-12-20 17:09:26 +0800

    update cs

--- Z.System/Z.Web/Z.Web/Controllers/HomeController.cs
+++ Z.System/Z.Web/Z.Web/Controllers/HomeController.cs
@@ -17,9 +17,28 @@ namespace Z.Web.Controllers
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
             //var t=new AdministratorsService().Create();
 
+            int[] _ar = { 10, 2, 3, 4, 0, 11 };
+            Array.Sort(_ar);
+            for (int i = 0; i < _ar.Length; i++)
+            {
+
+                for (int j = _ar.Length - 1; j >= 0; j--)
+                {
+                    var currentValue = _ar[0];
+                    var max = _ar[j];
+                    if (max > currentValue)
+                    {
+                        var min = _ar[j - 1];
+                        _ar[j-1] = currentValue;
+                        _ar[0] = min;
+                    }
+                }
+            }
+
+
             XmlUtility xm = new XmlUtility();
             xm.test();
-            
+
             Func<int, bool> f1 = IsNumberLessThen5;
             bool flag = f1(4);
 
