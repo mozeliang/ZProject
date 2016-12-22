22ace5b967e3038ebcd258d546a3bb8606f988ea
mzl <307096439@qq.com>
2016-12-22 17:05:35 +0800

    update all

---
+++ Z.Core.Service/Test/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.Core.Service.Test
+{
+    public class test
+    {
+        public void t() {
+            priv();
+        }
+
+        private void priv() { }
+    }
+}
